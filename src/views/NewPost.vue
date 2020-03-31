061f4bed3c8c966fa1493dca26057144c3566921
LorenaJeger <57949840+LorenaJeger@users.noreply.github.com>
2020-03-31 12:23:18 +0200

    Add files via upload

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,13 +1,17 @@
 <template>
   <form @submit.prevent="postImage" class="mb-5">
+    <input type="text" v-model="postTitle"/>
+    <br/><br/>
     <croppa :width="400" :height="400" v-model="imageData"></croppa>
     <button type="submit" class="btn btn-primary ml-2">Post image</button>
+    
   </form>
 </template>
 
 <script>
 import InstagramCard from "@/components/InstagramCard.vue";
 import store from "@/store.js";
+import { Posts } from '@/services'
 
 export default {
   data() {
@@ -15,34 +19,37 @@ export default {
   },
   name: "newpost",
   methods: {
-    postImage() {
+
+      postImage() {
+      
       this.imageData.generateBlob(blobData => {
         if (blobData != null) {
           // ako koristimo "/" u nazivu slike, Storage fino napravi direktorij.
           // Konkretno u ovom primjeru imat ćemo direktorij nazvan po mailu korisnika.
           // Slika će biti nazvana po trenutnom vremenu kako bi imali jedinstveni naziv slike.
           let imageName = this.userEmail + "/" + Date.now() + ".png";
-
+          
           storage
             .ref(imageName)
             .put(blobData)
             .then(result => {
               result.ref.getDownloadURL()
                 .then(url => {
-                  db.collection("posts")
-                    .add({
-                      email: this.userEmail,
-                      posted_at: Date.now(),
-                      url: url
-                    })
-                    .then(docRef => {
-                      console.log("Document written with ID: ", docRef.id);
-                      this.imageData = null;
-                      this.$router.push({name: "posts"})
-                    })
-                    .catch(e => {
-                      console.error("Error adding document: ", error);
-                    });
+
+              let post= {
+                user:this.userEmail,
+                posted_at:Date.now(),
+                url: url,
+                title:this.postTitle
+              }
+              
+                Posts.create(post)
+                .then(()=> {
+
+                 this.$router.push({name:"posts"})
+
+                })
+                
                 })
                 .catch(e=> {
                   console.error(e)
